483b2cce8dbfe88721f41af434cb39e3a74a0ce4
Badri <badrimishra7@gmail.com>
2026-05-19 22:47:51 -0700

    Deploy static export to gh-pages

---
+++ _next/static/hg2-0CZtPDnsnpkuGv4XP/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/immerse-ucla//_next/:path+",
+        "destination": "/immerse-ucla/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
